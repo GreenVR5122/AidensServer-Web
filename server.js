a381fa24a9f435f2f650d7a872ab8e85f0f4740d
GreenVR5122 <pebblepablo0@gmail.com>
2025-08-04 07:03:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
+const WebSocket = require('ws');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,20 +16,52 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Chat logic
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// Set up a WebSocket server for Minecraft plugin to connect
+const wss = new WebSocket.Server({ noServer: true });
 
-  socket.on('chat message', (msg) => {
-    // Broadcast to all clients
-    io.emit('chat message', msg);
+// Track Minecraft client connection
+let mcSocket = null;
+
+server.on('upgrade', (request, socket, head) => {
+  if (request.url === '/mc-ws') {
+    wss.handleUpgrade(request, socket, head, ws => {
+      mcSocket = ws;
+      console.log('Minecraft plugin connected');
+      
+      ws.on('message', message => {
+        // Broadcast Minecraft message to website clients
+        io.emit('chat message', `[MC] ${message.toString()}`);
+      });
+
+      ws.on('close', () => {
+        console.log('Minecraft plugin disconnected');
+        mcSocket = null;
+      });
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+// Website clients connect here
+io.on('connection', socket => {
+  console.log('Website client connected');
+
+  socket.on('chat message', msg => {
+    // Broadcast message to other website clients
+    io.emit('chat message', `[Web] ${msg}`);
+
+    // Also send to Minecraft if connected
+    if (mcSocket && mcSocket.readyState === WebSocket.OPEN) {
+      mcSocket.send(`[Web] ${msg}`);
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('Website client disconnected');
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
